6da1d9029071c503f3888bdf04aeadcab05278dc
Rohit Soni <Rsony.721@gmail.com>
2022-02-03 02:39:25 +0530

    Code for SignIn, signOut, getuserInfo and logout using firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private auth: AngularFireAuth) {}
 
-  constructor() { }
+  async signUp(email: string, password: string) {
+    return await this.auth.createUserWithEmailAndPassword(email, password);
+  }
+  async signIn(email: string, password: string) {
+    return await this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  async getUser() {
+    return await this.auth.authState;
+  }
+
+  async signOut() {
+    return await this.auth.signOut();
+  }
 }
