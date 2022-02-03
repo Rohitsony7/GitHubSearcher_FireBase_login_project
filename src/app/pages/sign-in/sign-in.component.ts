50c93d85d66b3654facbd3cd444bac9198924a41
Rohit Soni <Rsony.721@gmail.com>
2022-02-04 03:35:22 +0530

    added bootstrap in style, coded signin component, some header and page not found  css fix

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  constructor(
+    private ToastrService: ToastrService,
+    private AuthService: AuthService,
+    private Router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value;
+
+    // checking and validating email, password handled by firebase
+
+    this.AuthService.signIn(email, password)
+      .then((res) => {
+        this.Router.navigateByUrl('/');
+      })
+      .then((res) => {
+        this.ToastrService.success('signIn Success !');
+      })
+      .catch((err) => {
+        console.error(err.message);
+        this.ToastrService.error('SignIn failed');
+      });
   }
-
 }
