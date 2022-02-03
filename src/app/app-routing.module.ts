09ad6125ff6f7c7fa0e249d75bf11abdb422561b
Rohit Soni <Rsony.721@gmail.com>
2022-02-04 03:09:21 +0530

    Protracting routed using firebase and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
+import {
+  AngularFireAuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/compat/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['signin']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
+const routes: Routes = [
+  {
+    //if anyone already authorized than redirect to home
+    path: 'signin',
+    component: SignInComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome },
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
+  },
+  {
+    //if anyone is not authorized redirect to login
+    path: '',
+    component: HomeComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    // this(** or rest all the things) must be at the bottom
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'github-login-firebase';
+  constructor(private AuthService: AuthService) {
+    this.AuthService.getUser().subscribe({
+      next: (user) => {
+        console.log(user);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
