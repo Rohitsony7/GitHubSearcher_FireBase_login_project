e178ec62782967e006285ba4c28383b5be1a17f2
Rohit Soni <Rsony.721@gmail.com>
2022-02-04 01:37:58 +0530

    Finishing Home component ... :)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  user: any = null;
+  userName: any;
+  Error = null;
 
-  constructor() { }
+  constructor(
+    private GithubService: GithubService,
+    private ref: ChangeDetectorRef,
+    private ToastrService: ToastrService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleFind() {
+    this.GithubService.getUserDetails(this.userName).subscribe({
+      next: (user) => {
+        this.user = user;
+        //since this is an observable and the user details will be updating again and again so will use ref
+        this.ref.detectChanges();
+      },
+      error: (err) => {
+        console.log('>>> Error >>>', err);
+        this.ref.detectChanges();
+        this.ToastrService.error(err);
+      },
+      complete: () => console.log('Get user done...'),
+    });
   }
-
 }
