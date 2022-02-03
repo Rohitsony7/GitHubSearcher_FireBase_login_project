cbb83074ce73be346d2ca5b57cf288a35af9762a
Rohit Soni <Rsony.721@gmail.com>
2022-02-03 23:38:07 +0530

    Implemented header and repo logic and template, integerated services for user repos and firebase for header

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  styleUrls: ['./repos.component.css'],
 })
-export class ReposComponent implements OnInit {
+export class ReposComponent implements OnInit, OnChanges {
+  @Input() repoUrl!: string;
 
-  constructor() { }
+  repos: any = [];
 
-  ngOnInit(): void {
+  constructor(
+    private GithubService: GithubService,
+    private ref: ChangeDetectorRef,
+    private ToastrService: ToastrService
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges() {
+    if (this.repoUrl) {
+      this.GithubService.getUserRepo(this.repoUrl).subscribe({
+        next: (res) => {
+          this.repos = res;
+          this.ref.detectChanges();
+        },
+        error: (err) => {
+          console.log('>>> Error >>>', err);
+          this.ToastrService.error(err);
+        },
+        complete: () => console.log('Get user done...'),
+      });
+    }
   }
-
 }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  email: any = null;
+  constructor(
+    private AuthService: AuthService,
+    private ToastrService: ToastrService,
+    private Router: Router
+  ) {
+    // latest rxJs with subscribe
+    //handled Observable
+    AuthService.getUser().subscribe({
+      next: (user) => {
+        this.email = user?.email;
+      },
+      error: (err) => {
+        console.log('>>> Error >>>', err);
+        this.ToastrService.error(err);
+      },
+      complete: () => console.log('Get user done...'),
+    });
   }
 
+  ngOnInit(): void {}
+
+  //handled promise
+  handleSignOut() {
+    this.AuthService.signOut()
+      .then((res) => {
+        console.log('sign out success', res);
+      })
+      .then(() => {
+        this.Router.navigateByUrl('/signin');
+      })
+      .then(() => {
+        this.ToastrService.info('Login again to continue.');
+      })
+      .then(() => {
+        this.email = null;
+      })
+      .catch((err) => {
+        console.error('error >>>>', err);
+        this.ToastrService.error('Something went wrong !!');
+      })
+      .finally(() => {
+        console.log('sign out mehtod done...');
+      });
+  }
 }
